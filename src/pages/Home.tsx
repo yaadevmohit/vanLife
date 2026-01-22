84c0c4c689d020b919ab13bad15826045e1712c4
yaadevmohit <mohityadav530@gmail.com>
2026-01-22 17:20:41 -0500

    home component update with tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,26 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-        <div className="home-container">
-            <h1>You got the travel plans, we got the travel vans.</h1>
-            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            <Link to="vans">Find your van</Link>
+        <div className="home-container min-h-96 text-white px-11 py-6 ">
+            <h1 className="text-4xl font-bold leading-10.2">You got the travel plans, we got the travel vans.</h1>
+            <p className="leading-6 my-6">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <Link 
+                to="vans"
+                className="
+                    inline-block
+                    text-center
+                    bg-[#FF8C38]
+                    w-full
+                    mt-7
+                    py-3
+                    text-white
+                    font-bold
+                    rounded-lg
+                    
+                "
+            >
+                Find your van
+            </Link>
         </div>
     )
 };
