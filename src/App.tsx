b1b1ebc0cdffc8da1d8ee9138ca59fbcb9e8a5b2
yaadevmohit <mohityadav530@gmail.com>
2026-01-22 14:31:07 -0500

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
