51fbe3fe7676955ab49f803eec1b5631f7d6733a
yaadevmohit <mohityadav530@gmail.com>
2026-01-25 18:34:20 -0500

    aria label update for van tile

--- src/pages/Vans.tsx
+++ src/pages/Vans.tsx
@@ -24,10 +24,13 @@ export default function Vans() {
 
     const vanElements = vans.map((van: Van) => (
         <div key={van.id} >
-            <Link to={`/vans/${van.id}`}>
-            <img src={van.imageUrl} className="w-full rounded-md" />
+            <Link 
+                to={`/vans/${van.id}`}
+                aria-label={`View details for ${van.name}, priced at $${van.price} per day`}
+            >
+            <img src={van.imageUrl} className="w-full rounded-md" alt={`Image of ${van.name}`}/>
             <div className="van-info">
-                <h3>{van.name}</h3>
+                <h2 className="text-2xl">{van.name}</h2>
                 <p>${van.price}<span>/day</span></p>
             </div>
             <i className={`van-type ${van.type} selected`}>{van.type}</i>
