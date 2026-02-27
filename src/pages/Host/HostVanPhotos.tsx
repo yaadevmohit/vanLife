3aa8a2def83ccd0c03b8c7c84ca7cb70b3c07e8f
yaadevmohit <mohityadav530@gmail.com>
2026-02-26 20:06:01 -0500

    hostVanDetail sub-routes implemented

--- src/pages/Host/HostVanPhotos.tsx
+++ src/pages/Host/HostVanPhotos.tsx
@@ -1,5 +1,12 @@
+import { useVanData } from "../../hooks/useVanData"
 export default function HostVanPhotos() {
+  const {vanData} = useVanData()
   return (
-    <div>HostVanPhotos</div>
+    vanData ?
+    <section className="flex gap-4">
+      <img src={vanData.imageUrl} className="max-w-32"alt={`image of ${vanData.name}`}/>
+      <img src={vanData.imageUrl} className="max-w-32"alt={`image of ${vanData.name}`}/>
+    </section>
+    : <h2>Loading...</h2>
   )
 }
--- src/pages/Host/HostVanPricing.tsx
+++ src/pages/Host/HostVanPricing.tsx
@@ -1,5 +1,9 @@
+import { useVanData } from "../../hooks/useVanData"
 export default function HostVanPricing() {
+  const {vanData} = useVanData()
   return (
-    <div>HostVanPricing</div>
+    vanData ? 
+    <p className="text-2xl"><span className="font-bold">${vanData.price}</span>/day</p>
+    : <h2>Loading...</h2>
   )
 }
